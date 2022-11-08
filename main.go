b36632afc15d69e33a464dc1aa7a0b6695570c7c
Anna-Katharina Wickert <wickert@cs.tu-darmstadt.de>
2022-11-08 22:12:56 +0100

    Remove types to show argument type inference

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	fmt.Printf(solar2k.Print())
 	fmt.Printf(solar3k.Print())
 	fmt.Printf(windwest.Print())
-	fmt.Printf("our first generic function: %s\n", business.PrintGeneric[business.Solar](solar2k))
-	fmt.Printf("our first generic function with wind: %s\n", business.PrintGeneric[business.Wind](windwest))
+	fmt.Printf("our first generic function: %s\n", business.PrintGeneric(solar2k))
+	fmt.Printf("our first generic function with wind: %s\n", business.PrintGeneric(windwest))
 
 	// Print functions for 01_04
 	ss := []business.Solar{solar2k, solar3k}
