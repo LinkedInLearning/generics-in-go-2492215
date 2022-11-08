197e3f997380c51958bfa95268fb6f23bbef34ad
Anna-Katharina Wickert <wickert@cs.tu-darmstadt.de>
2022-11-08 22:04:48 +0100

    Add simple cost function for screencast
    The function Cost will serve as an example for argument type inference later.
    As the implementation is straight-forward, we providethe function and the interface as starting point for this chapter.

--- business/cost.go
+++ business/cost.go
@@ -1,5 +1,9 @@
 package business
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
 // Cost computes the netto cost for a costumer for Solar and returns the value
 func (s Solar) Cost() float64 {
 	return s.Netto * 0.4
@@ -8,4 +12,15 @@ func (s Solar) Cost() float64 {
 // Cost computes the netto cost for a costumer for Wind and returns the value
 func (w Wind) Cost() float64 {
 	return w.Netto * 0.3
+}
+
+// Number is either a floating point number or an integer
+type Number interface {
+	constraints.Float | constraints.Integer
+}
+
+// Cost multiplies usage with netto to compute the cost.
+func Cost[T Number](usage, netto T) T {
+	cost := usage * netto
+	return cost
 }
